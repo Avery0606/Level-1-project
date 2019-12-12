b746257fd23e5a855d35f89ae1e364cdaebbd5d8
JIExit <1064614313@qq.com>
2019-12-12 15:05:36 +0800

    点赞动画

--- pages/stu/tieba/tieba.js
+++ pages/stu/tieba/tieba.js
@@ -4,6 +4,11 @@ var that = this;
 var movableImageSideLength = 0.15;//可拖动图片边长
 Page({
   data: {
+    id:[0,1,2,3],
+    dianZanSrc: ["../../../images/点赞.png", "../../../images/点赞.png", "../../../images/点赞.png"],
+    commentnum:0,
+    likesnum:[0,0,0],
+    likecolor:["","",""],
     x: wx.getSystemInfoSync().windowWidth * (1.01 - movableImageSideLength),
     y:10000,
     movableareaH: 100 * (0.94 * wx.getSystemInfoSync().windowHeight - movableImageSideLength * wx.getSystemInfoSync().windowWidth) / wx.getSystemInfoSync().windowHeight + "vh", 
@@ -22,8 +27,8 @@ Page({
   onShareAppMessage: function () {},
   // abc函数实现拖动图片并自动靠边
   abc:function(event){
-    let cuX = event.changedTouches[0].pageX
-    let cuY = event.changedTouches[0].pageY - movableImageSideLength*wx.getSystemInfoSync().windowWidth / 2;
+    let cuX = event.changedTouches[0].clientY
+    let cuY = event.changedTouches[0].clientY - movableImageSideLength*wx.getSystemInfoSync().windowWidth / 2;
     let middleX = wx.getSystemInfoSync().windowWidth/2;
     let setX = wx.getSystemInfoSync().windowWidth * (1.01 - movableImageSideLength)
     if (cuX > middleX||cuX <= middleX) {
@@ -32,5 +37,25 @@ Page({
         y: cuY
       })
     }
+  },
+  dianzan:function(event){
+    let Id = event.currentTarget.id;
+    let LIKESNUM = "likesnum["+Id+"]";
+    let LIKECOLOR = "likecolor[" + Id +"]";
+    let DIANZANSRC = "dianZanSrc["+Id+"]";
+    if(this.data.likesnum[Id] == 0){
+      this.setData({
+        [LIKESNUM]:1,
+        [LIKECOLOR]:"#e97e3f",
+        [DIANZANSRC]:"../../../images/点赞ed.png"
+      })
+    }
+    else{
+      this.setData({
+        [LIKESNUM]: 0,
+        [LIKECOLOR]: "",
+        [DIANZANSRC]:"../../../images/点赞.png"
+      })
+    };
   }
 })
